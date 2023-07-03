535581ea27175c4054d07208f080cca5967664f4
BFieguth <bfieguth@uoguelph.ca>
2023-07-03 15:16:53 -0400

    Added calculations for ME function

--- script/NASEM_functions/ration_balancing_equations.py
+++ script/NASEM_functions/ration_balancing_equations.py
@@ -103,19 +103,74 @@ def calculate_Du_MiN_VTnln_g(An_RDPIn, Rum_DigNDFIn, Rum_DigStIn):
     return Du_MiN_VTnln_g
 
 
-# def calculate_Mlk_Prod_comp():
+# def calculate_Mlk_Prod_comp(Dt_IdArgRUPIn):
+# # Need to calculate in diet:
+# # 1. Dt_IdArgRUPIn
 
-#     Mlk_NP_g = mPrt_Int + Abs_Arg_g*mPrt_k_Arg + Abs_His_g*mPrt_k_His + Abs_Ile_g*mPrt_k_Ile + 
-# 	Abs_Leu_g*mPrt_k_Leu + Abs_Lys_g*mPrt_k_Lys + Abs_Met_g*mPrt_k_Met + Abs_Phe_g*mPrt_k_Phe +
-# 	Abs_Thr_g*mPrt_k_Thr + Abs_Trp_g*mPrt_k_Trp + Abs_Val_g*mPrt_k_Val + Abs_neAA_g*mPrt_k_NEAA +
-#      Abs_OthAA_g*mPrt_k_OthAA + Abs_EAA2b_g*mPrt_k_EAA2 + An_DEInp*mPrt_k_DEInp + (An_DigNDF-17.06)*mPrt_k_DigNDF + 
-#      (An_DEStIn+An_DEFAIn+An_DErOMIn)*mPrt_k_DEIn_StFA + An_DENDFIn*mPrt_k_DEIn_NDF + (An_BW-612)*mPrt_k_BW
+
+
+#     # Mlk_NP_g =   + Abs_His_g*mPrt_k_His + Abs_Ile_g*mPrt_k_Ile + 
+# 	# Abs_Leu_g*mPrt_k_Leu + Abs_Lys_g*mPrt_k_Lys + Abs_Met_g*mPrt_k_Met + Abs_Phe_g*mPrt_k_Phe +
+# 	# Abs_Thr_g*mPrt_k_Thr + Abs_Trp_g*mPrt_k_Trp + Abs_Val_g*mPrt_k_Val + Abs_neAA_g*mPrt_k_NEAA +
+#     #  Abs_OthAA_g*mPrt_k_OthAA + Abs_EAA2b_g*mPrt_k_EAA2 + An_DEInp*mPrt_k_DEInp + (An_DigNDF-17.06)*mPrt_k_DigNDF + 
+#     #  (An_DEStIn+An_DEFAIn+An_DErOMIn)*mPrt_k_DEIn_StFA + An_DENDFIn*mPrt_k_DEIn_NDF + (An_BW-612)*mPrt_k_BW
     
-#     Mlk_NP = Mlk_NP_g / 1000                                                                # Line 2211
+#     K_305RHA_MlkTP = 1.0                                            # Line 2115, A scalar to adjust the slope if needed.  Assumed to be 1. MDH
+#     An_305RHA_MlkTP = 280                                           # Line 34, kg/305d The mean value from the meta data.
+#     f_mPrt_max = 1.0 + K_305RHA_MlkTP * (An_305RHA_MlkTP/280 - 1)
 
-#     Mlk_Prod_comp = 4.541 + 11.13*Mlk_NP + 2.648*Mlk_Fat + 0.1829 * An_DEIn - 0.06257*(An_LactDay_MlkPred-137.1) 
+#     mPrt_Int_src = -97                                          # Line 2078-2096, These are the NRC defaults but two other sets of values are also available in the code
+#     mPrt_k_BW_src = -0.4201
+#     mPrt_k_DEInp_src = 10.79
+#     mPrt_k_DigNDF_src = -4.595
+#     mPrt_k_DEIn_StFA_src = 0                                    #DEStIn + DErOMIn + DEFAIn
+#     mPrt_k_DEIn_NDF_src = 0                                     #DENDFIn
+#     mPrt_k_Arg_src = 0
+#     mPrt_k_His_src = 1.675	
+#     mPrt_k_Ile_src = 0.885
+#     mPrt_k_Leu_src = 0.466
+#     mPrt_k_Lys_src = 1.153	
+#     mPrt_k_Met_src = 1.839
+#     mPrt_k_Phe_src = 0
+#     mPrt_k_Thr_src = 0
+#     mPrt_k_Trp_src = 0.0
+#     mPrt_k_Val_src = 0
+#     mPrt_k_NEAA_src = 0                                         #NEAA.  Phe, Thr, Trp, and Val not considered.
+#     mPrt_k_OthAA_src = 0.0773                                   #NEAA + unused EAA.  Added for NRC eqn without Arg as slightly superior.
+#     mPrt_k_EAA2_src = -0.00215 
+
+#     mPrtmx_Arg = -mPrt_k_Arg_src**2 / (4 * mPrt_k_EAA2_src)
+    
+#     mPrtmx_Arg2 = mPrtmx_Arg * f_mPrt_max                             # Line 2149
+
+#     Arg_mPrtmx = -mPrt_k_Arg_src / (2 * mPrt_k_EAA2_src)
+
+#     # Calculate scaled linear EAA coeff and a common squared coefficient
+#     mPrt_k_Arg = -(2 * math.sqrt(mPrtmx_Arg2)**2 - mPrt_Arg_0.1 * mPrtmx_Arg2) - 2 * mPrtmx_Arg2 / (Arg_mPrtmx * 0.1)
+
+#     MiTPArgProf = 5.47
+    
+#     SI_dcMiCP = 80                              				# Digestibility coefficient for Microbial Protein (%) from NRC 2001
+#     fMiTP_MiCP = 0.824          		                        # Fraction of MiCP that is True Protein; from Lapierre or Firkins
+#     Du_MiN_g = calculate_Du_MiN_NRC2021_g()                     # This value requires another function to calculate, in the ration balancer it is probably easiest to run that function first then pass the Du_MiN_g value as an input
+    
+#     # Du_IdArgMic #
+#     Du_MiCP_g = Du_MiN_g * 6.25
+#     Du_MiTP_g = fMiTP_MiCP * Du_MiCP_g
+#     Du_ArgMic = Du_MiTP_g * MiTPArgProf / 100                     # Line 1573
+#     Du_IdArgMic = Du_ArgMic * SI_dcMiCP / 100                     # Line 1691
+
+#     Abs_Arg_g = Du_IdArgMic + Dt_IdArgRUPIn                     # Line 1757, 1714, 1703 
+    
+#     # BIG EQUATION #
+#     Mlk_NP_g = mPrt_Int_src + Abs_Arg_g * mPrt_k_Arg                # Line 2187-2191
+#     #######################################################################
+   
+#     Mlk_NP = Mlk_NP_g / 1000                                                                # Line 2210
+
+#     Mlk_Prod_comp = 4.541 + 11.13*Mlk_NP + 2.648*Mlk_Fat + 0.1829 * An_DEIn - 0.06257*(An_LactDay_MlkPred-137.1)                # Line 2275
 #     + 2.766e-4*(An_LactDay_MlkPred-137.1)^2 + 1.603e-6*(An_LactDay_MlkPred-137.1)^3 - 7.397e-9*(An_LactDay_MlkPred-137.1)^4 
-#     + 1.567*(An_Parity_rl-1)    
+#     + 1.567*(An_Parity_rl-1)        
 
 #     return Mlk_Prod_comp
 
