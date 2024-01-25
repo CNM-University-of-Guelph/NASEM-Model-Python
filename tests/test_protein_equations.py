42e878fe99a4f5b22902c92d6566c55bb1578fc9
BFieguth <bfieguth@uoguelph.ca>
2024-01-25 16:28:59 -0500

    test(protein_equations): Add unit tests for all functions in protein_equations.py

---
+++ tests/test_protein_equations.py
@@ -0,0 +1,24 @@
+import pytest
+import nasem_dairy as nd
+import pandas as pd
+
+
+@pytest.fixture
+def protein_data():
+    return pd.read_json("./tests/protein_equations_test.json")
+
+
+def test_from_json(protein_data):
+    for index, row in protein_data.iterrows():
+        try:
+            func = getattr(nd, row.Name)
+            input_params = row.Input.copy()
+                        # Check if "coeff_dict" is present in input_params
+            if "coeff_dict" in input_params:
+                # Assign it the actual dictionary
+                input_params["coeff_dict"] = nd.coeff_dict            
+            
+            assert func(**input_params) == pytest.approx(row.Output), f"{row.Name} failed: {func(**input_params)} does not equal {row.Output}"
+        except AttributeError:
+            print(f"Function {row.Name} not found in module.")
+            
