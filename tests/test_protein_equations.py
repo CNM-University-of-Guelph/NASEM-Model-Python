814c08862e09d9ef18d2776ba4afe1eeb0634044
David Innes <dave.j.innes@gmail.com>
2024-01-30 15:07:23 -0500

    tests: unit testing all previous work

--- tests/test_protein_equations.py
+++ tests/test_protein_equations.py
@@ -2,15 +2,14 @@ import pytest
 import nasem_dairy as nd
 import pandas as pd
 
-# TODO Add tests for wrapper functions calculate_An_data_initial and calculate_An_data_complete
 
 @pytest.fixture
-def animal_data():
-    return pd.read_json("./tests/animal_equations_test.json")
+def protein_data():
+    return pd.read_json("./tests/protein_equations_test.json")
 
 
-def test_from_json(animal_data):
-    for index, row in animal_data.iterrows():
+def test_from_json(protein_data):
+    for index, row in protein_data.iterrows():
         try:
             func = getattr(nd, row.Name)
             input_params = row.Input.copy()
@@ -22,3 +21,4 @@ def test_from_json(animal_data):
             assert func(**input_params) == pytest.approx(row.Output), f"{row.Name} failed: {func(**input_params)} does not equal {row.Output}"
         except AttributeError:
             print(f"Function {row.Name} not found in module.")
+            
