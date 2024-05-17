a1211f6976ad1debf757fc28e391bf5bd713d70d
Braeden Fieguth <bfieguth@uoguelph.ca>
2024-05-17 13:31:43 -0400

    style(methane_equations): Apply Google style

--- src/nasem_dairy/NASEM_equations/methane_equations.py
+++ src/nasem_dairy/NASEM_equations/methane_equations.py
@@ -1,9 +1,11 @@
 # Methane equations
+# import nasem_dairy.NASEM_equations.methane_equations as methane
+
 def calculate_CH4out_g(An_GasEOut: float, coeff_dict: dict) -> float:
     """
     CH4out_g: Methane production (g/d)
     """
-    CH4out_g = An_GasEOut / coeff_dict['En_CH4'] * 1000     # g methane/d, Line 3240
+    CH4out_g = An_GasEOut / coeff_dict['En_CH4'] * 1000  # g methane/d, Line 3240
     return CH4out_g
 
 
@@ -11,7 +13,7 @@ def calculate_CH4out_L(CH4out_g: float, coeff_dict: dict) -> float:
     """
     CH4out_L: Methane production (L/d)
     """
-    CH4out_L = CH4out_g / 1000 * coeff_dict['CH4vol_kg']    # L/d, Line 3241
+    CH4out_L = CH4out_g / 1000 * coeff_dict['CH4vol_kg']  # L/d, Line 3241
     return CH4out_L
 
 
@@ -19,7 +21,7 @@ def calculate_CH4g_Milk(CH4out_g: float, Mlk_Prod: float) -> float:
     """
     CH4g_Milk: Methane intensity (g CH4 / kg milk)
     """
-    CH4g_Milk = CH4out_g / Mlk_Prod # g/kg, Line 3242
+    CH4g_Milk = CH4out_g / Mlk_Prod  # g/kg, Line 3242
     return CH4g_Milk
 
 
@@ -27,5 +29,5 @@ def calculate_CH4L_Milk(CH4out_L: float, Mlk_Prod: float) -> float:
     """
     CH4L_Milk: Methane intensity (g CH4 / kg milk)
     """
-    CH4L_Milk = CH4out_L / Mlk_Prod # L/kg, Line 3243
+    CH4L_Milk = CH4out_L / Mlk_Prod  # L/kg, Line 3243
     return CH4L_Milk
