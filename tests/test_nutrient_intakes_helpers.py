48163adf6db1cd5f3934e0b2978e5fa4f4ef3c83
Braeden Fieguth <bfieguth@uoguelph.ca>
2024-05-21 10:57:32 -0400

    feat(test_nutrient_intakes): Use default coeff_dict when not defined in test JSON

--- tests/test_nutrient_intakes_helpers.py
+++ tests/test_nutrient_intakes_helpers.py
@@ -1,28 +1,41 @@
-import pytest
+import os
+
 import pandas as pd
-import nasem_dairy as nd
+import pytest
 import numpy as np
+
+import nasem_dairy as nd
 
 @pytest.fixture
 def nutrient_intake_dfs():
-    return pd.read_json("./tests/nutrient_intakes_helpers_test.json")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    return pd.read_json(os.path.join(script_dir, 
+                                     "nutrient_intakes_helpers_test.json"))
+
 
 def compare_values_with_tolerance(val1, val2, rtol=1e-3, atol=1e-2):
-    if isinstance(val1, (list, np.ndarray, pd.Series)) and isinstance(val2, (list, np.ndarray, pd.Series)):
+    if (isinstance(val1, (list, np.ndarray, pd.Series)) and 
+        isinstance(val2, (list, np.ndarray, pd.Series))
+        ):
         np.testing.assert_allclose(val1, val2, rtol=rtol, atol=atol)
     else:
         np.testing.assert_allclose([val1], [val2], rtol=rtol, atol=atol)
+
 
 def compare_dicts_with_tolerance(dict1, dict2, rtol=1e-3, atol=1e-2):
     for key in dict1:
         if key in dict2:
             try:
-                compare_values_with_tolerance(dict1[key], dict2[key], rtol=rtol, atol=atol)
+                compare_values_with_tolerance(dict1[key], dict2[key], 
+                                              rtol=rtol, atol=atol
+                                              )
             except AssertionError as e:
-                print(f"Mismatch found for key '{key}': {dict1[key]} (expected) vs {dict2[key]} (actual)")
+                print(f"Mismatch found for key '{key}': {dict1[key]} (expected)"
+                      f" vs {dict2[key]} (actual)")
                 raise e
         else:
             raise KeyError(f"Key {key} not found in both dictionaries")
+
 
 def test_from_json_dict(nutrient_intake_dfs):
     for index, row in nutrient_intake_dfs.iterrows():
@@ -32,6 +45,10 @@ def test_from_json_dict(nutrient_intake_dfs):
         # Convert input DataFrame if present
         if 'df' in input_params:
             input_params['df'] = pd.DataFrame(input_params['df'])
+        if (('coeff_dict' in input_params) and 
+            (input_params['coeff_dict'] == None)
+            ):
+            input_params['coeff_dict'] = nd.coeff_dict
         
         # Determine if the expected output is a DataFrame or a dictionary
         if isinstance(row.Output, dict):
@@ -63,7 +80,8 @@ def test_from_json_dict(nutrient_intake_dfs):
             print("Result DataFrame:")
             print(result)
             
-            pd.testing.assert_frame_equal(result, expected_output, rtol=1e-3, atol=1e-2)
+            pd.testing.assert_frame_equal(result, expected_output, 
+                                          rtol=1e-3, atol=1e-2)
 
 if __name__ == "__main__":
-    pytest.main()
+    pytest.main(['./tests/test_nutrient_intakes_helpers.py'])
