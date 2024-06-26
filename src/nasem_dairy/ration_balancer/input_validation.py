a4057aa4260a13d0831d63d037d4b4887afc04dd
Braeden Fieguth <bfieguth@uoguelph.ca>
2024-06-26 16:04:43 -0400

    feat(validate_user_diet): Add function to check user_diet is properly formatted

--- src/nasem_dairy/ration_balancer/input_validation.py
+++ src/nasem_dairy/ration_balancer/input_validation.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
+def validate_user_diet(user_diet: pd.DataFrame) -> pd.DataFrame:
+    """
+    Check that the use_diet DataFrame is formatted properly and contains the
+    required data for execute_model.
+    """
+    if not isinstance(user_diet, pd.DataFrame):
+        raise TypeError("user_diet must be a pandas DataFrame")
+    expected_columns = ["Feedstuff", "kg_user"]
+    if list(user_diet.columns) != expected_columns:
+        raise ValueError(f"user_diet must have the columns {expected_columns}")
+    user_diet["kg_user"] = pd.to_numeric(user_diet["kg_user"], errors="coerce")
+    if user_diet["kg_user"].isna().any():
+        raise ValueError("kg_user column must contain only numeric values")
+    if not user_diet["Feedstuff"].apply(lambda x: isinstance(x, str)).all():
+        raise ValueError("Feedstuff column must contain only string values")
+    user_diet = (user_diet  # Combine duplicate Feedstuff entries
+                 .groupby("Feedstuff", as_index=False)
+                 .agg({"kg_user": "sum"})
+                 )
+    user_diet = user_diet.dropna()
+    if user_diet.empty:
+        raise ValueError(f"user_diet is an empty DataFrame")
+    return user_diet
+
+
 def validate_animal_input(animal_input):
     type_mapping = {
             'An_Parity_rl': float,
@@ -34,6 +61,37 @@ def validate_animal_input(animal_input):
                 raise ValueError(f"Error converting {key}: {value} to {type_mapping[key].__name__}") from e
         else:
             corrected_input[key] = value
+# - An_StatePhys and An_Breed are valid strings
+# 
+
+
 
     return corrected_input
 
+
+def validate_equation_selection(equation_selection):
+    pass
+
+
+def validate_feed_library_df(feed_library_df):
+    pass
+
+
+def validate_coeff_dict(coeff_dict):
+    pass
+
+
+def validate_infusion_input(infusion_input):
+    pass
+
+
+def validate_MP_NP_efficiency_input(MP_NP_efficiency_input):
+    pass
+
+
+def validate_mPrt_coeff_list(mPrt_coeff_list):
+    pass
+
+
+def validate_f_Imb(f_Imb):
+    pass
