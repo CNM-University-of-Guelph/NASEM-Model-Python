1be3ee7234e8498a43f38e84303b951f4291ed9c
BFieguth <bfieguth@uoguelph.ca>
2024-07-18 08:04:25 -0400

    fix(calculate_Dt_AAIn): Remove duplicate of calculate_Dt_AAIn from milk_equations module

--- src/nasem_dairy/__init__.py
+++ src/nasem_dairy/__init__.py
@@ -79,8 +79,7 @@ from nasem_dairy.NASEM_equations.milk_equations import (
     calculate_MlkNP_AbsNEAA,
     calculate_MlkNP_AbsOthAA,
     calculate_Trg_Mlk_NP,
-    calculate_Mlk_AA_TP,
-    calculate_Dt_AAIn
+    calculate_Mlk_AA_TP
 )
 
 from nasem_dairy.NASEM_equations.nutrient_intakes import (
